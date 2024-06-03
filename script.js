198eb12f8ea75f6f7ce42d246a24d4e26feddf74
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-06-03 14:30:46 +0300

    Add zabolevaniya page

---
+++ script.js
@@ -0,0 +1,24 @@
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    // tab.ariaSelected = false;
+    tab.setAttribute("aria-selected", false);
+  });
+  event.currentTarget.setAttribute("aria-selected", true);
+  // find the associated tabPanel and show it!
+  const { id } = event.currentTarget;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener("click", handleTabClick)
+);

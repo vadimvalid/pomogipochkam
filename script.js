4bdb80c4bbe1690c26b06dbf676259174d2ac3e6
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-06-04 11:38:15 +0300

    Update tabs logic

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const tabs = document.querySelectorAll(".section .tabs");
-const tabButtons = document.querySelectorAll('.section .tabs [role="tab"]');
-const tabPanels = document.querySelectorAll('.section .tabs [role="tabpanel"]');
 
-function handleTabClick(event) {
-  // hide all tab panels
-  tabPanels.forEach((panel) => {
-    panel.hidden = true;
-  });
-  // mark all tabs as unselected
-  tabButtons.forEach((tab) => {
-    // tab.ariaSelected = false;
-    tab.setAttribute("aria-selected", false);
-  });
-  event.currentTarget.setAttribute("aria-selected", true);
-  // find the associated tabPanel and show it!
-  const { id } = event.currentTarget;
-  const tabPanel = document.querySelector(`[aria-labelledby="${id}"]`);
-  tabPanel.hidden = false;
-}
+tabs.forEach((tab) => {
+  const tabButtons = tab.querySelectorAll('[role="tab"]');
+  const tabPanels = tab.querySelectorAll('[role="tabpanel"]');
 
-tabButtons.forEach((button) => {
-  button.addEventListener("click", handleTabClick);
+  function handleTabClick(event) {
+    // hide all tab panels
+    tabPanels.forEach((panel) => {
+      panel.hidden = true;
+    });
+    // mark all tabs as unselected
+    tabButtons.forEach((tab) => {
+      // tab.ariaSelected = false;
+      tab.setAttribute("aria-selected", false);
+    });
+    event.currentTarget.setAttribute("aria-selected", true);
+    // find the associated tabPanel and show it!
+    const { id } = event.currentTarget;
+    const tabPanel = tab.querySelector(`[aria-labelledby="${id}"]`);
+    tabPanel.hidden = false;
+  }
+
+  tabButtons.forEach((button) =>
+    button.addEventListener("click", handleTabClick)
+  );
 });
